7edeef1f273564b6ca1cf9029b40b2d74ec27d7a
Renzo <91288365+Renzo-Henrique@users.noreply.github.com>
2022-10-07 10:59:51 -0300

    Add files via upload
    Falta ver se aresta e vetor_aresta sao no mesmo arquivo
    Implementar o sort

--- src/aresta.c
+++ src/aresta.c
@@ -1,13 +1,99 @@
-#include "posicao.h"
-#include "vetorDistancia.h"
+#include "../headers/aresta.h"
 
-struct vetorAresta{
-    Aresta** vet;
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct aresta
+#include "../headers/posicao.h"
+
+struct aresta_st
 {
     Posicao* a;
     Posicao* b;
-    double dist;
+    float dist;
 };
+
+Aresta *aresta_init(Posicao* a, Posicao* b){
+    Aresta* aresta = (Aresta*)malloc(sizeof(Aresta));
+    aresta->a = a;
+    aresta->b = b;
+    aresta->dist =posicao_distancia(a,b);
+
+    return aresta;
+}
+
+Posicao* aresta_getA(Aresta *aresta){
+    return aresta->a;
+}
+
+Posicao* aresta_getB(Aresta *aresta){
+    return aresta->b;
+}
+
+float aresta_getDist(Aresta *aresta){
+    return aresta->dist;
+}
+
+void aresta_libera(Aresta *aresta){
+    free(aresta);
+}
+
+/**
+ * comeco do vetor de aresta
+ * 
+ */
+#include "../headers/vetor.h"
+#include "../headers/assertr.h"
+struct vetorAresta_st{
+    Aresta** vet;
+    int qtd;
+};
+vetorAresta* vetorAresta_init(Vetor* posicoes){
+    vetorAresta* vetor = (vetorAresta*)malloc(sizeof(vetorAresta));
+
+    vetor->vet = (Aresta*)malloc(sizeof(Aresta));
+
+    //quantidade de arestas eh uma PA: N(N-1)/2?
+    int qtdPosicoes = vetor_qtd_elementos(posicoes); 
+    vetor->qtd = qtdPosicoes*(qtdPosicoes - 1)/2;
+
+
+    int i=0 , j=0, k = 0;
+    //inicializa as arestas da "Diagonal Principal pra cima"
+    for(i =0; i < qtdPosicoes; i++){
+
+        assertx(k < vetor->qtd, "Index do vetor de aresta ultrapassou limite");
+
+        for(j = i+1; j < qtdPosicoes; j++){
+
+            /**
+             * Nao eh possivel obter index do vetor de aresta de forma linear ao vetor posicoes
+             * O k vai ser o index de cada vetor
+             */
+            vetor->vet[k] = aresta_init(vetor_get_index(posicoes,i), vetor_get_index(posicoes,j));
+
+            k++;
+            
+        }
+    }
+
+    assertx(k == vetor->qtd, "Index do vetor de aresta ultrapassou limite");
+
+
+    return vetor;
+
+}
+//falta fazer
+void vetorAresta_sort(vetorAresta* vetor);
+
+Aresta* vetorAresta_get_Index(vetorAresta* vetor, int index){
+    return vetor->vet[index];
+}
+int vetorAresta_get_Qtd(vetorAresta* vetor){
+    return vetor->qtd;
+}
+void vetorAresta_libera(vetorAresta* vetor){
+    free(vetor->vet);
+    free(vetor);
+}
+
