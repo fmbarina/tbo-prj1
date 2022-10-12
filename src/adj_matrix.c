ef7944252ed7dc6f96b55f64c3b52b0bc21a681a
Felipe M Barina <EMAIL>
2022-10-12 19:50:54 -0300

    Ops

--- src/adj_matrix.c
+++ src/adj_matrix.c
@@ -1,9 +1,10 @@
 #include "adj_matrix.h"
 
+#include <stdlib.h>
 #include "bytes.h"
 
-/* Economizando linhas rs */
-#define swap(x, y) ({ unsigned long tmp = x; x = y; y = tmp; })
+/* Economizando linhas rs TODO: tem como fazer isso sem ext gcc? */
+#define swap(x, y) __extension__({ unsigned long tmp = x; x = y; y = tmp; })
 
 struct adj_matrix_st
 {
@@ -37,7 +38,7 @@ unsigned char adj_mat_get(Adj_matrix *mat, unsigned long i, unsigned long j)
 
 void adj_mat_set(Adj_matrix *mat, unsigned long i, unsigned long j, unsigned char v)
 {
-    if (i == j) return 0;
+    if (i == j) return;
     if (i > j) swap(i, j);
 
     unsigned char *vet = bytes_contents(mat->mat);
